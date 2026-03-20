6d4fc7cd66347f857e72ecdb31d3891e260d0dc3
shruthi.chintamaneni66 <shruthi.chintamaneni66@gmail.com>
2026-03-20 09:11:18 +0530

    UC11: Object-Oriented palindrome service

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+class PalindromeService {
+    public boolean check(String word) {
+        String reversed = new StringBuilder(word).reverse().toString();
+        return word.equalsIgnoreCase(reversed);
+    }
+}
 
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        PalindromeService service = new PalindromeService();
 
-        System.out.print("Enter a sentence: ");
-        String input = sc.nextLine();
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
 
-        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
-        String reversed = new StringBuilder(cleaned).reverse().toString();
-
-        if (cleaned.equals(reversed)) {
-            System.out.println("Palindrome (Ignore case & spaces)");
+        if (service.check(word)) {
+            System.out.println("Palindrome (OOP Service)");
         } else {
             System.out.println("Not Palindrome");
         }

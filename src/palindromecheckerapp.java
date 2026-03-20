6440dc06938f8459cfa73023a63280bfdee034bd
shruthi.chintamaneni66 <shruthi.chintamaneni66@gmail.com>
2026-03-20 08:57:09 +0530

    UC4: Loop for multiple checks

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -4,15 +4,24 @@ public class palindromecheckerapp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a word: ");
-        String word = sc.nextLine();
+        while (true) {
+            System.out.print("Enter a word: ");
+            String word = sc.nextLine();
 
-        String reversed = new StringBuilder(word).reverse().toString();
+            String reversed = new StringBuilder(word).reverse().toString();
 
-        if (word.equalsIgnoreCase(reversed)) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not Palindrome");
+            if (word.equalsIgnoreCase(reversed)) {
+                System.out.println("Palindrome");
+            } else {
+                System.out.println("Not Palindrome");
+            }
+
+            System.out.print("Continue? (yes/no): ");
+            String choice = sc.nextLine();
+
+            if (choice.equalsIgnoreCase("no")) {
+                break;
+            }
         }
     }
 }

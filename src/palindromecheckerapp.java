8b125e0ed8e57248a3a5c8b5afc1123d5dc1979b
shruthi.chintamaneni66 <shruthi.chintamaneni66@gmail.com>
2026-03-20 09:14:52 +0530

    UC13: Performance comparison of palindrome methods

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,30 +1,37 @@
 import java.util.*;
 
-interface palindromeStrategy {
-    boolean isPalindrome(String word);
-}
-
-class SimpleStrategy implements PalindromeStrategy {
-    public boolean isPalindrome(String word) {
-        String rev = new StringBuilder(word).reverse().toString();
-        return word.equalsIgnoreCase(rev);
-    }
-}
-
-class RecursiveStrategy implements PalindromeStrategy {
-    public boolean isPalindrome(String word) {
-        return check(word, 0, word.length() - 1);
-    }
-
-    private boolean check(String str, int start, int end) {
-        if (start >= end) return true;
-        if (str.charAt(start) != str.charAt(end)) return false;
-        return check(str, start + 1, end - 1);
-    }
-}
-
 public class PalindromeCheckerApp {
+
+    public static boolean method1(String word) {
+        return word.equals(new StringBuilder(word).reverse().toString());
+    }
+
+    public static boolean method2(String word) {
+        int left = 0, right = word.length() - 1;
+        while (left < right) {
+            if (word.charAt(left++) != word.charAt(right--)) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter word: ");
+        String word = sc.nextLine();
+
+        long start1 = System.nanoTime();
+        method1(word);
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        method2(word);
+        long end2 = System.nanoTime();
+
+        System.out.println("Method1 Time: " + (end1 - start1));
+        System.out.println("Method2 Time: " + (end2 - start2));
+    }
+} void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter word: ");

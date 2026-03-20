29696a5c2a8e5b5560312269c209d2192a0bdf24
shruthi.chintamaneni66 <shruthi.chintamaneni66@gmail.com>
2026-03-20 09:09:52 +0530

    UC10: Case-insensitive & space-ignored palindrome

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,23 +1,18 @@
 import java.util.Scanner;
 
-public class palindromecheckerapp {
-
-    public static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) return true;
-
-        if (str.charAt(start) != str.charAt(end)) return false;
-
-        return isPalindrome(str, start + 1, end - 1);
-    }
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a word: ");
-        String word = sc.nextLine();
+        System.out.print("Enter a sentence: ");
+        String input = sc.nextLine();
 
-        if (isPalindrome(word, 0, word.length() - 1)) {
-            System.out.println("Palindrome (Recursive)");
+        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        if (cleaned.equals(reversed)) {
+            System.out.println("Palindrome (Ignore case & spaces)");
         } else {
             System.out.println("Not Palindrome");
         }
